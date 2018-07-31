a5a73bc8061c6fea0e8ab7f9dd6fcd4f19136327
zhangrj <zhangrj@ethicall.cn>
2018-07-31 10:49:21 +0800

    fix csdn

--- itspider/spiders/csdn.py
+++ itspider/spiders/csdn.py
@@ -1,70 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import arrow
-import json
-import scrapy
+import logging
 
 from itspider.items import ArticleItem
 from itspider.utils import md5
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+logger = logging.getLogger(__name__)
 
 
-class CsdnSpider(scrapy.Spider):
+class CsdnSpider(CrawlSpider):
     name = 'csdn'
     allowed_domains = ['blog.csdn.net']
     start_urls = (
         'https://blog.csdn.net/',
     )
-    article_list_url = 'https://blog.csdn.net/api/articles?type=more&category={category}&shown_offset={shown_offset}'
-    custom_settings = {
-        'DOWNLOAD_DELAY': 0.05
-    }
-    categories = {}
+    rules = (
+        Rule(LinkExtractor(allow=('nav/(.*?)'))),
+        Rule(LinkExtractor(allow=('(.*?)/article/details/(.*?)',)), callback='parse_article'),
+    )
+    download_delay = 1
 
-    def __init__(self, *args, **kwargs):
-        super(CsdnSpider, self).__init__(*args, **kwargs)
-
-    def parse(self, response):
-        navs = response.xpath('//nav[@id="nav"]/div/div/ul/li/a')
-        for nav in navs[3:]:
-            title = nav.xpath('text()').extract_first()
-            url = nav.xpath('@href').extract_first()
-            key = url.split('/')[-1]
-            self.categories[key] = title
-
-        for category in self.categories.keys():
-            now = ''.join(str(arrow.utcnow().float_timestamp).split('.'))
-            url = self.article_list_url.format(category=category, shown_offset=now)
-            req = scrapy.Request(url, self.article_list)
-            req.meta['category'] = category
-            yield req
-
-    def article_list(self, response):
-        category = response.meta['category']
-        response = json.loads(response.body)
-        shown_offset = response.get('shown_offset', 0)
-
-        for article in response.get('articles', []):
-            url = article['url']
-            req = scrapy.Request(url, self.article)
-            req.meta['category'] = category
-            req.meta['title'] = article['title']
-            req.meta['user'] = article['user_name']
-            yield req
-
-        url = self.article_list_url.format(category=category, shown_offset=shown_offset)
-        req = scrapy.Request(url, self.article_list)
-        req.meta['category'] = category
-        yield req
-
-    def article(self, response):
+    def parse_article(self, response):
         item = ArticleItem()
+        logger.info(response.url)
         item['index'] = 'article-{}'.format(self.name)
         item['document_id'] = md5(response.url)
         item['document_type'] = self.name
         item['source'] = response.url
-        item['classify'] = self.categories[response.meta['category']]
-        item['title'] = response.meta['title']
-        item['user'] = response.meta['user']
+        item['title'] = response.xpath('//h1[@class="title-article"]//text()').extract_first().strip()
+        item['user'] = response.xpath('//a[@id="uid"]//text()').extract_first().strip()
         body = response.xpath('//article//text()').extract()
         item['body'] = '\n'.join(b for b in body if ''.join(b.split()))
         yield item
